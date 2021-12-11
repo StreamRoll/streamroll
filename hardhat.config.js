6ad68b70df59ad6b2e41379f8b7d1da6102d5c73
dallascat <39738814+chrisalexadams@users.noreply.github.com>
2021-12-11 03:45:42 +0000

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,29 +1,35 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({path: '.env'});
 
-const pk = "";
-const mainnetEndpoint = "";
-const rinkebyEndpoint = "";
-const kovanEndpoint = "";
+const { INFURA_PROJECT_ID, PRIVATE_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.7",
   networks: {
+    localhost: {
+      url: "http://localhost:8545",
+    },
     rinkeby: {
-      url:rinkebyEndpoint,
-      accounts: [`0x${pk}`]
+      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts:[`0x${PRIVATE_KEY}`]
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts:[`0x${PRIVATE_KEY}`]
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts:[`0x${PRIVATE_KEY}`]
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts:[`0x${PRIVATE_KEY}`]
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts:[`0x${PRIVATE_KEY}`]
     }
   }
-  //TESTING
-  // mocha: {
-  //   timeout:100000
-  // },
-  // networks: {
-  //   hardhat: {
-  //     forking: {
-  //       url: mainnetEndpoint
-  //     }
-  //   }
-  // },
 };
 
 
